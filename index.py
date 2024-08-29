af713a084c4d9dae18b22ea91ee3ae191a4fb2d9
Salman Malik <162824871+Salman7292@users.noreply.github.com>
2024-08-29 19:55:54 +0500

    Update index.py

--- index.py
+++ index.py
@@ -363,7 +363,7 @@ st.markdown(page_bg_img, unsafe_allow_html=True)
 # Sidebar configuration
 with st.sidebar:
     # Display logo image
-    st.image("logo4.png", use_column_width=True)
+    st.image("https://raw.githubusercontent.com/Salman7292/Brain-Tumor-Detection-Project/29fdf15114e9a1f81d02441619176acec3f55dbd/Logo4.png", use_column_width=True)
 
     # Adding a custom style with HTML and CSS for sidebar
     st.markdown("""
